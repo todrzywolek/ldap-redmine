ce30d11fbb01e1223796999fb15971dbee053749
Tomasz Odrzywolek <tomeko92@gmail.com>
2020-05-09 09:55:48 +0200

    reading issue only by admin

--- src/main/java/pl/edu/agh/ldap/issues/IssueReader.java
+++ src/main/java/pl/edu/agh/ldap/issues/IssueReader.java
@@ -1,6 +1,11 @@
 package pl.edu.agh.ldap.issues;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 public class IssueReader {
 
@@ -11,7 +16,26 @@ public class IssueReader {
     }
 
     public Collection<IssueDao> getAllIssues() {
-        return repository.getAll();
+        Collection<IssueDao> issues = repository.getAll();
+        Collection<? extends GrantedAuthority> authorities = SecurityContextHolder.getContext().getAuthentication().getAuthorities();
+        boolean isAdmin = false;
+        for (GrantedAuthority a : authorities) {
+            if ("ROLE_MANAGERS".equals(a.getAuthority())) {
+                isAdmin = true;
+                break;
+            }
+        }
+        List<IssueDao> selectedIssues = new ArrayList<>();
+        for (IssueDao issue : issues) {
+            if (IssueTracker.ADMIN == (issue.getTracker())) {
+                if (isAdmin) {
+                    selectedIssues.add(issue);
+                }
+            } else {
+                selectedIssues.add(issue);
+            }
+        }
 
+        return selectedIssues;
     }
 }
